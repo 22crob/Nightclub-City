272dbf596ecec0fad90389967e95e41b963c14c2
22crob <22.crob@gmail.com>
2026-09-20 23:35:35 -0400

    Remove obsolete canvas prototype module

--- src/animations.js
+++
@@ -1,8 +0,0 @@
-export function easeOutCubic(t) {
-  return 1 - Math.pow(1 - t, 3);
-}
-
-export function pulse(time, speed = 1, min = 0, max = 1) {
-  const n = (Math.sin(time * speed) + 1) * .5;
-  return min + (max - min) * n;
-}
