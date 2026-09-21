4d096c3f20e013342e2c6110a9f673afa707e53b
22crob <22.crob@gmail.com>
2026-09-20 23:32:02 -0400

    Add Phaser rebuild foundation

---
+++ src/config/constants.js
@@ -0,0 +1,27 @@
+export const CLUB = Object.freeze({
+  gridWidth: 15,
+  gridHeight: 13,
+  tileWidth: 96,
+  tileHeight: 48,
+  originX: 720,
+  originY: 150
+});
+
+export const DEPTH = Object.freeze({
+  floor: 0,
+  objectBase: 1000,
+  guestBase: 2000,
+  overlay: 10000
+});
+
+export const COLORS = Object.freeze({
+  background: 0x090815,
+  floorA: 0x1b1931,
+  floorB: 0x16142a,
+  grid: 0x393459,
+  wallLeft: 0x171724,
+  wallRight: 0x222033,
+  purple: 0x8c5cff,
+  pink: 0xff4ecb,
+  cyan: 0x52e7ff
+});
